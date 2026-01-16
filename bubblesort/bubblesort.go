d07a7cd6011d54d5fe33dea1650d034a74b1db6f
frederikbassler-cell <frederikbassler@gmail.com>
2026-01-16 15:31:12 +0000

    1 aufgabe gelöst

--- bubblesort/bubblesort.go
+++ bubblesort/bubblesort.go
@@ -4,11 +4,41 @@ package bubblesort
 // wenn das linke größer ist als das rechte.
 // Gibt true zurück, wenn mindestens ein Tausch stattgefunden hat.
 func BubbleUp(arr []int) bool {
-	// TODO
-	return false
+
+	swapped := false
+
+	for i, n := range arr {
+
+		if i+1 < len(arr) {
+
+			if n > arr[i+1] {
+
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				swapped = true
+			}
+		}
+	}
+
+	return swapped
 }
 
 // BubbleSort sortiert die übergebene Liste mittels des Bubble-Sort-Algorithmus.
 func BubbleSort(arr []int) {
-	// TODO
+
+	for d := 0; d <= len(arr); d++ {
+
+
+		for i, n := range arr {
+
+			if i+1 < len(arr) {
+
+				if n > arr[i+1] {
+
+					arr[i], arr[i+1] = arr[i+1], arr[i]
+
+				}
+			}
+		}
+	}
+
 }
